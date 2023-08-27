d7cb3b31effcc6d1b47b2653ece98b297f0fca15
gabrieljuniodasilvabispo <joaovitor.bispo080@gmail.com>
2023-08-27 16:36:26 -0300

    design desktop quase todo feito

---
+++ script.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('nav');
+
+const controller = true
+
+menu.addEventListener('click', (e) => {
+    nav.style.display = 'block';
+
+});
