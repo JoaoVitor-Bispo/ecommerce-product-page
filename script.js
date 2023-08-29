ccd29f9193ef4f0a5f88e4b96b5177deba36888c
gabrieljuniodasilvabispo <joaovitor.bispo080@gmail.com>
2023-08-29 16:55:07 -0300

    função de click na imagem, algumas alterações na responsividade(incompleto)

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ const directions = [...document.querySelectorAll('.directions')]
 const images_slider = document.querySelector('.images-slider')
 const thumbnails = document.querySelector('.thumbnails-container')
 const labels = [...document.getElementsByTagName('label')]
+const radios = [...document.querySelectorAll('.radios')]
+const closeBtn = document.querySelector('.close-button')
+
+images_slider.addEventListener('click', (e) => {
+
+    const newSlider = images_slider.cloneNode(true)
+    const div_container = document.createElement('div')
+    const newClose_button = newSlider.children[0]
+
+    newSlider.children[1].className = 'new-directions new-left'
+    newSlider.children[6].className = 'new-directions new-right'
+
+    newClose_button.classList.add('new-button')
+    newSlider.classList.add('newslider')
+    div_container.classList.add('new-container')
+
+    div_container.appendChild(newSlider)
+    document.documentElement.appendChild(div_container)
+
+    newClose_button.addEventListener('click', (e) => div_container.remove())
+})
