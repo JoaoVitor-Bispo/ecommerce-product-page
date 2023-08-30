6e3793f018c83cf3e7b96ad271de28baa0b36412
gabrieljuniodasilvabispo <joaovitor.bispo080@gmail.com>
2023-08-30 17:36:04 -0300

    algumas interações javascript

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const nav = document.querySelector('nav');
 const directions = [...document.querySelectorAll('.directions')]
 const images_slider = document.querySelector('.images-slider')
 const thumbnails = document.querySelector('.thumbnails-container')
-const labels = [...document.getElementsByTagName('label')]
-const radios = [...document.querySelectorAll('.radios')]
 const closeBtn = document.querySelector('.close-button')
+const cart = document.querySelector('.cart-icon')
+const quantity_product = document.querySelector('.quantity')
+const quantity_buttons = [...document.querySelectorAll('.quantity-button')]
+const addCart = document.querySelector('.add-cart')
+const card = document.querySelector('.card')
+const card_content = document.querySelector('.card-content-box')
 
 images_slider.addEventListener('click', (e) => {
 
@@ -25,3 +29,40 @@ images_slider.addEventListener('click', (e) => {
 
     newClose_button.addEventListener('click', (e) => div_container.remove())
 })
+
+let controller = 0
+
+quantity_buttons.map((element, index) => {
+    element.addEventListener('click', (e) => {
+        if(index == 0) {
+            if(controller < 1) return false
+            
+            controller -= 1
+        }
+        else {
+            controller += 1
+        }
+        quantity_product.innerHTML = `${controller}`
+    })
+})
+
+cart.addEventListener('click', (e) => card.style.display == 'none' ? card.style.display = 'block' : card.style.display = 'none')
+
+addCart.addEventListener('click', (e) => {
+    if(controller > 0) {
+        card_content.style.display = 'flex'
+        document.querySelector('.empty-message').style.display = 'none'
+
+        document.querySelector('.final-quantity').innerHTML = `${controller}`
+        document.querySelector('.final-value').innerHTML = `$${controller * 125}`    
+    }
+})
+
+document.querySelector('.delete').addEventListener('click', e => {
+    card_content.style.display = 'none'
+    document.querySelector('.empty-message').style.display = 'block'
+})
+
+
+
+
